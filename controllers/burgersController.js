577f42fee51e197f16dd3f2e8a8b7901504877e1
Phil S <philipstubbs13@gmail.com>
2018-03-10 18:32:09 -0600

    added config to gitignore file

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,25 +1,47 @@
 // Import the model (burger.js) to use its database functions.
 var db = require("../models");
 
+//Require express
+var express = require("express");
+
 module.exports = function(app) {
   // Create all our routes and set up logic within those routes where required.
   //GET route to get burgers from database.
   app.get("/", function(req, res) {
     db.Burger.findAll({
-    }).then(function(result) {
-      res.render("index", { burgers: result });
+    }).then(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
     });
   }); 
 
   //POST route to create/add a burger.
-  // app.post("/api/burgers", function(req, res) {
+  app.post("/api/burgers", function(req, res) {
+    console.log("Burger Data:");
+    console.log(req.body);
+    console.log("Burger name: " + req.body.burger_name);
+    db.Burger.create({
+      burger_name: req.body.burger_name
+    }).then(function(result) {
+      res.json({ id: result.insertId });
+    });
+  });
 
-  // });
-
-  // //PUT route to update burger devoured state.
-  // app.put("/api/burgers/:id", function(req, res) {
-
-  // });
+  //PUT route to update burger devoured state.
+  app.put("/api/burgers/:id", function(req, res) {
+    db.Burger.update({
+      devoured: req.body.devoured
+    }, {
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbResult) {
+      res.redirect("/");
+    });
+});
 
   // //DELETE route to throw away a burger.
   // app.delete("/api/burgers/:id", function(req, res) {
